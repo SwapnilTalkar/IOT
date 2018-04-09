8aed16b2adc70f1b50047c0f8b86366c0ba9d5fa
SwapnilTalkar <swapnil.r.talkar@gmail.com>
2018-04-09 23:47:36 +0530

    dfgdf

--- app/src/main/java/com/example/sachin/electricityusage/constants/Constants.java
+++ app/src/main/java/com/example/sachin/electricityusage/constants/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
     public static String MAIN_URL="http://192.168.0.102/Electricity-Units/";
     public static String FETCH_DATA=MAIN_URL+"fetchdata.php";
     public static String MULTI_LINE_DATA=MAIN_URL+"getData.php";
-    public static String REAL_TIME_LINE_DATA=MAIN_URL+"realTimeData.php"g;
+    public static String REAL_TIME_LINE_DATA=MAIN_URL+"realTimeData.php";
 
 }
